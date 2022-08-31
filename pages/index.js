7bae843e50f8eccdd042ee87f649b33b10d25aac
Federico Grandi <fgrandi30@gmail.com>
2022-08-31 23:43:16 +0000

    chore: trigger ci deployment

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your New Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (This is a sample website - you'll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
