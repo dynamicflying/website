13452baa7f6c75f5328e803f1f65cfb2a3294aed
Federico <fgrandi30@gmail.com>
2023-01-16 01:35:40 +0100

    fix: fix image build

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image';
+import ExportedImage from 'next-image-export-optimizer';
 import Link from 'next/link';
-import logo from '../public/assets/logo.png';
+import logo from '../public/images/logo.png';
 
 export default function Header() {
   return (
     <div className="flex text-white h-20 p-5 border-b-solid border-b-2 border-b-slate-600">
       <div className="flex items-center justify-center ml-5">
-        <Image src={logo} alt="Logo" width={80} height={80} />
+        <ExportedImage src={logo} alt="Logo" width={80} height={80} />
       </div>
       <div className="flex-1 flex items-center justify-center gap-[7rem] text-lg">
         <div className="nav-link">
--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   basePath: '/website',
   images: {
-    unoptimized: true,
+    loader: 'custom',
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  env: {
+    nextImageExportOptimizer_imageFolderPath: 'public/images',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="none"` to all <ExportedImage> components.
+    //
+    // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
+    // forget to set `placeholder="none"`, you'll see 404 errors for the missing
+    // placeholder images in the console.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
   },
 };
