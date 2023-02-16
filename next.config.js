ab13c39ca15b02ea5b7d66d3515a416f17be2662
Federico <fgrandi30@gmail.com>
2023-02-16 16:47:02 +0100

    chore: remove image exporter

--- next.config.js
+++ next.config.js
@@ -2,23 +2,6 @@
 module.exports = {
   basePath: process.env.BASE_PATH || '/website',
   images: {
-    loader: 'custom',
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-  env: {
-    nextImageExportOptimizer_imageFolderPath: 'public/images',
-    nextImageExportOptimizer_exportFolderPath: 'out',
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-
-    // If you do not want to use blurry placeholder images, then you can set
-    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-    // `placeholder="none"` to all <ExportedImage> components.
-    //
-    // If nextImageExportOptimizer_generateAndUseBlurImages is false and you
-    // forget to set `placeholder="none"`, you'll see 404 errors for the missing
-    // placeholder images in the console.
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
+    unoptimized: true,
   },
 };
