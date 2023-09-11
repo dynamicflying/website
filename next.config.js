1651b8f1e39de39fb7bf13e61b132202b69bbf05
Federico Grandi <fgrandi30@gmail.com>
2023-09-11 18:57:25 +0200

    chore: set up Netlify previews (#130)
    * chore: use config file in favor of next export
    
    * chore: add netlify config
    
    * ci: remove current PR preview workflow
    
    * chore: fix build path in Netlify config
    
    * chore: disable Next Netlify plugin
    
    * chore: actually, use Netlify plugin
    
    * fix: change default base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: process.env.BASE_PATH || '/website',
+  basePath: process.env.BASE_PATH || '',
+  output: (process.env.OUTPUT || undefined) ? process.env.OUTPUT : undefined,
   images: {
     unoptimized: true,
   },
